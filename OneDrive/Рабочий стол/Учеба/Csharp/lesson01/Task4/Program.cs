3936fb7d79f13d67be26d76ec1a15f68f15c45d7
Margo <r.pruss@yandex.ru>
2024-03-08 21:41:25 +0300

    lesson01:task4

---
+++ OneDrive/Рабочий стол/Учеба/Csharp/lesson01/Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿int a = 100;
+int b = 7;
+int c = 50;
+int d = 4;
+int e = 10;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}if (d > max)
+{
+    max = d;
+}if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
