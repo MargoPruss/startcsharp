f77af7d3931461cc10575e322d64be761298c00d
Margo <r.pruss@yandex.ru>
2024-03-11 21:46:19 +0300

    Sem2

---
+++ OneDrive/Рабочий стол/Учеба/Csharp/Sem2/Sem2_Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и возводит вторую цифру этого числа 
+// в степень, равную третьей цифре.
+
+// Примеры
+// 487 => 8^7 = 2 097 152
+// 254 => 5^4 = 625
+// 617 => 1
+
+Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100 && num <= 999)
+{
+    int secondDigit = num / 10 % 10;
+    int numDegree = num % 10;
+    int result = 1;
+    int count = 0;
+    while (count < numDegree)
+    {
+        result *= secondDigit; // result = result * secondDigit
+        count++;
+        
+    }
+    Console.WriteLine (result);
+}
+else
+{
+    Console.WriteLine("Ошибка ввода");
+}
---
+++ OneDrive/Рабочий стол/Учеба/Csharp/Sem2/Sem2_Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и удаляет вторую цифру этого числа.
+// Примеры
+// a = 256  => 26
+// a = 891  => 81
+
+Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100 && num <= 999 || num >= 999 && num <= -100)
+{
+int firstDigit = num / 100;
+int lastDigit = num % 10;
+int result = firstDigit * 10 + lastDigit;
+
+Console.WriteLine(result);
+}
+else 
+{
+    Console.WriteLine("Ошибка ввода");
+}
