1fbf1b720f59e487fe21c9ed4f1e7991e4a3ab3e
Margo <r.pruss@yandex.ru>
2024-04-01 21:36:43 +0300

    Sem7

---
+++ OneDrive/Рабочий стол/Учеба/Csharp/Sem7/Sem7_Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// Указание
+// Использовать рекурсию.
+// Пример
+// 123 => 6
+// 63 => 9
+
+
+int SumOfDigitNum(int num)
+{
+    if(num == 0) return 0;
+    return num%10 + SumOfDigitNum(num / 10);
+}
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SumOfDigitNum(number));
---
+++ OneDrive/Рабочий стол/Учеба/Csharp/Sem7/Sem7_Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Считать строку с консоли, содержащую латинские буквы. Вывести на экран согласные буквы этой строки.
+// Указание
+// Использовать рекурсию. Не использовать цикл.
+// Пример
+// “hello” => h l l 
+// “World” => W r l d 
+// “Hello world!” => H l l w r l d
+
+void PrintConsonants (string str)
+{
+    if(str.Length == 0) return;
+    string chars = "aeiouy";
+    if(char.IsAsciiLetter(str[0]) && !chars.Contains(str[0]))
+        Console.Write($"{str[0]} ");
+    PrintConsonants(str.Substring(1));
+}
+
+
+Console.WriteLine("Введите строку: ");
+string str = Console.ReadLine();
+
+PrintConsonants(str);
